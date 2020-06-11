56deeee3590551ff2e97a29885d719068cfd7fa7
vivian1311 <elvis212332@gmail.com>
2020-06-11 14:34:58 +0800

    finish

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,57 +1,53 @@
-import paho.mqtt.client as paho
-import time
 import matplotlib.pyplot as plt
 import numpy as np
-import serial
-
-# MQTT broker hosted on local machine
+import paho.mqtt.client as paho
+import time
 mqttc = paho.Client()
+
 # Settings for connection
 host = "localhost"
-topic = "Mbed"
+topic= "Mbed"
+port = 1883
 
-# signal
-x = np.arange(0,2,0.005)
-y = np.arange(0,2,0.005)
-z = np.arange(0,2,0.005)
-tilt = np.arange(0,2,0.005)
-t = np.arange(0,20,0.1)
+x = np.arange(0, 1, 1/18)
+y = np.arange(0, 1, 1/18)
+z = np.arange(0, 1, 1/18)
+t = np.arange(0, 1, 1/18)
+tilt = np.arange(0, 18, 1)
 
-
+i = 0
 # Callbacks
 def on_connect(self, mosq, obj, rc):
-      print("Connected rc: " + str(rc))
-def on_message(mosq, obj, msg):
-      data_input = str(msg.payload).split()
-      for i in range (0, 200):
-            x[i] = float(data_input[i])
-      for i in range (200, 400):
-            y[i] = float(data_input[i])
-      for i in range (400, 600):
-            z[i] = float(data_input[i])
-      for i in range (600, 800):
-            tilt[i] = float(data_input[i])
+    print("Connected rc: " + str(rc))
 
-      # ploting
-      print("start ploting\n")
-      fig, ax = plt.subplots(2, 1)
-      ax[0].plot(t, x, color = "red", linestyle = "-", label = "x")
-      ax[0].plot(t, y, color = "blue", linestyle = "-", label = "y")
-      ax[0].plot(t, z, color = "green", linestyle = "-", label = "z")
-      ax[0].legend(loc = 'upper left', frameon = False)
-      ax[0].set_xlabel('Time')
-      ax[0].set_ylabel('Acc Vector')
-      
-      ax[1].plot(t,y_t)
-      ax[1].set_xlabel('Time')
-      ax[1].set_ylabel('Amplitude')
-      plt.show()
+def on_message(mosq, obj, msg):
+    global i
+    i += 1
+    print("[Received] Topic: " + msg.topic + ", Message: " + str(msg.payload) + "\n")
+    data = float(msg.payload)
+    print(data)
+    print(i)
+    if (i-1)%4 == 0:
+        print("nx = ", int((i-1)/4))
+        x[int((i-1)/4)] = data
+        print("x = ", x[(i-1)/4])
+    elif (i-1)%4 == 1:
+        print("ny = ", int((i-1)/4))
+        y[int((i-1)/4)] = data
+    elif (i-1)%4 == 2:
+        print("nz = ", int((i-1)/4))
+        z[int((i-1)/4)] = data
+    elif (i-1)%4 == 3:
+        print("nt = ", int((i-1)/4))
+        tilt[int((i-1)/4)] = data
+    
 
 
 def on_subscribe(mosq, obj, mid, granted_qos):
-      print("Subscribed OK")
+    print("Subscribed OK")
+
 def on_unsubscribe(mosq, obj, mid, granted_qos):
-      print("Unsubscribed OK")
+    print("Unsubscribed OK")
 
 # Set callbacks
 mqttc.on_message = on_message
@@ -65,6 +61,22 @@ mqttc.connect(host, port=1883, keepalive=60)
 mqttc.subscribe(topic, 0)
 
 
-
-# Loop forever, receiving messages
-mqttc.loop_forever()
+while True:
+    mqttc.loop()
+    if i >= 72:
+        print(x)
+        print(y)
+        print(z)
+        fig, ax = plt.subplots(2, 1)
+        ax[0].plot(t, x, color = "red", linestyle = "-", label = "x")
+        ax[0].plot(t, y, color = "blue", linestyle = "-", label = "y")
+        ax[0].plot(t, z, color = "green", linestyle = "-", label = "z")
+        ax[0].legend(loc = 'upper left', frameon = False)
+        ax[0].set_xlabel('Time')
+        ax[0].set_ylabel('Acc Vector')
+        
+        ax[1].plot(t,tilt)
+        ax[1].set_xlabel('Time')
+        ax[1].set_ylabel('Amplitude')
+        plt.show()
+        break
